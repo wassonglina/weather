4bf1576280863d0ee693ee2bff8929f6d6b483c1
Lina Wassong <lina.wassong@gmail.com>
2022-05-12 21:30:07 -0700

    Add Secrets

---
+++ weather/Model/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  Weather Pal
+//
+//  Created by Lina on 3/31/22.
+//
+
+import Foundation
+
+struct Secrets {
+   static let openWeatherAppID = "2be507915c85d5ef6860278c106b24b4"
+}
